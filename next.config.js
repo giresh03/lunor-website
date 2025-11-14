dfcec94688b134ec4f0abca2640e4d072989f0e9
Girish <gireshkumar1414@gmail.com>
2025-11-14 22:33:38 +0530

    Remove webpack config to test build

--- next.config.js
+++ next.config.js
@@ -7,21 +7,6 @@ const nextConfig = {
   },
   compress: true,
   poweredByHeader: false,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      }
-    }
-    // Force single React instance
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      react: require.resolve('react'),
-      'react-dom': require.resolve('react-dom'),
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig

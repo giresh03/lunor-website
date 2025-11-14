5a8ab3b0ffb4d9b951b919b403eb102869929b19
Girish <gireshkumar1414@gmail.com>
2025-11-14 22:31:47 +0530

    Fix webpack config: Remove resolutions, add React alias plugins

--- next.config.js
+++ next.config.js
@@ -7,18 +7,27 @@ const nextConfig = {
   },
   compress: true,
   poweredByHeader: false,
-  webpack: (config, { isServer }) => {
+  experimental: {
+    webpackBuildWorker: true,
+  },
+  webpack: (config, { isServer, webpack }) => {
     if (!isServer) {
       config.resolve.fallback = {
         ...config.resolve.fallback,
         fs: false,
       }
     }
+    // Force single React instance
     config.resolve.alias = {
       ...config.resolve.alias,
       react: require.resolve('react'),
       'react-dom': require.resolve('react-dom'),
     }
+    // Ignore node modules that might have React
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(/^react$/, require.resolve('react')),
+      new webpack.NormalModuleReplacementPlugin(/^react-dom$/, require.resolve('react-dom'))
+    )
     return config
   },
 }
